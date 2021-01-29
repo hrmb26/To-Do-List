ff555e81ca61549ecb7ee1ea6dbe9f5887a9ec79
Hector Mata <hrmb26@gmail.com>
2021-01-28 20:38:12 -0400

    version 0.2

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # To-Do-List
+
+## TO-DO List (hehe)
+
+* Local storage
+* Export to xls, csv or txt
+* Delete all button
+
+## Version History
+
+**0.2**
+* Line-through to elements in the list
+* Can add items with Enter key now
+* Text now wraps for every item list
+* List items and close buttons are now equidistant of each other
+
+**0.1**
+
+-Initial Release
--- script.js
+++ script.js
@@ -10,6 +10,16 @@ for (i = 0; i < listElements.length; i++) {
 	};
 }
 
+for (i = 0; i < listElements.length; i++) {
+	listElements[i].onclick = function() {
+		if (this.style.textDecoration == 'line-through') {
+			this.style.textDecoration = 'none';
+		} else {
+			this.style.textDecoration = 'line-through';
+		}
+	};
+}
+
 function addItem() {
 	let value = document.getElementById('textbox_id').value;
 	if (value == '') {
@@ -18,7 +28,7 @@ function addItem() {
 		let node = document.createElement('LI');
 		let textnode = document.createTextNode(value);
 		let closeButton = document.createElement('span');
-		let text = document.createTextNode('x');
+		let text = document.createTextNode('\u00D7');
 		node.appendChild(textnode);
 		node.appendChild(closeButton);
 		closeButton.appendChild(text);
@@ -26,6 +36,17 @@ function addItem() {
 		document.getElementById('todo_list').appendChild(node);
 		document.getElementById('textbox_id').value = '';
 	}
+
+	for (i = 0; i < listElements.length; i++) {
+		listElements[i].onclick = function() {
+			if (this.style.textDecoration == 'line-through') {
+				this.style.textDecoration = 'none';
+			} else {
+				this.style.textDecoration = 'line-through';
+			}
+		};
+	}
+
 	for (i = 0; i < listElements.length; i++) {
 		close[i].onclick = function() {
 			let div = this.parentElement;
@@ -33,3 +54,10 @@ function addItem() {
 		};
 	}
 }
+
+document.getElementById('textbox_id').addEventListener('keyup', function(event) {
+	event.preventDefault();
+	if (event.keyCode === 13) {
+		document.getElementById('addButton').click();
+	}
+});
